e5fcfbe0aeedbb694c9806113d43e59b979e1692
Nirvan Mishra <127642231+Nirvan-Mishra-09@users.noreply.github.com>
2024-02-04 18:25:07 -0500

    Update prior_en_df.v

--- prior_en_df.v
+++ prior_en_df.v
@@ -1,12 +1,12 @@
 // Priority Encoder using Data flow modelling
-module prior_en_df(en, in, o);
+module prior_en_df(en, d, q);
   //declaring input
   input en;
-  input [7:0]in;
+  input [7:0]d;
   //declaring output
-  output [2:0]o;
+  output [2:0]q;
   //assigning the output 
-  assign o[2] = in[4] | in[5] | in[6] | in[7]&en;
-  assign o[1] = in[2] | in[3] | in[6] | in[7]&en;
-  assign o[0] = in[1] | in[3] | in[5] | in[7]&en;
+  assign q[2] = d[4] | d[5] | d[6] | d[7]&en;
+  assign q[1] = d[2] | d[3] | d[6] | d[7]&en;
+  assign q[0] = d[1] | d[3] | d[5] | d[7]&en;
 endmodule
