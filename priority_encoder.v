2b76bc915456713cb18a7cd254de4833585d221a
Nirvan Mishra <127642231+Nirvan-Mishra-09@users.noreply.github.com>
2024-02-04 18:31:24 -0500

    Update priority_encoder.v

--- priority_encoder.v
+++ priority_encoder.v
@@ -1,24 +1,24 @@
 // Priority Encoder using Behavioral Modelling
-module priorityencoder(en, i, y);
+module priorityencoder(en, d, q);
 
 // declaring input
 input en; //declaring enable
-input [7:0]i; // since 8x3 priority encoder, i.e 8 input lines have 3 output lines.
-output reg [2:0]y;
+input [7:0]d; // since 8x3 priority encoder, i.e 8 input lines have 3 output lines.
+output reg [2:0]q;
 
-always @(en, i)
+always @(en, d)
 begin
 if (en == 1)
 	begin
-	if (i[7] == 1) y = 3'b111;
-	else if(i[6] == 1) y = 3'b110;
-	else if(i[5] == 1) y = 3'b101;
-	else if(i[4] == 1) y = 3'b100;
-	else if(i[3] == 1) y = 3'b011;
-	else if(i[2] == 1) y = 3'b010;
-	else if(i[1] == 1) y = 3'b001;
-	else y = 3'b000;
+	if (d[7] == 1) q = 3'b111;
+	else if(d[6] == 1) q = 3'b110;
+	else if(d[5] == 1) q = 3'b101;
+	else if(d[4] == 1) q = 3'b100;
+	else if(d[3] == 1) q = 3'b011;
+	else if(d[2] == 1) q = 3'b010;
+	else if(d[1] == 1) q = 3'b001;
+	else q = 3'b000;
 	end
-else y = 3'bzzz;
+else q = 3'bzzz;
 end
 endmodule
