20efd877d1bdf1244ac356685f64d7ca050f75ec
Nirvan Mishra <127642231+Nirvan-Mishra-09@users.noreply.github.com>
2024-02-04 18:26:13 -0500

    Update priority_en_gl.v

--- priority_en_gl.v
+++ priority_en_gl.v
@@ -1,16 +1,16 @@
 // Priority Encoder using Gate-Level modelling
-module priority_en_gl(en, in, o);
+module priority_en_gl(en, d, q);
   input en;
-  input [7:0]in;
-  output [2:0]o;
+  input [7:0]d;
+  output [2:0]q;
   
   wire x, y, z;
   
-  or out2(x, in[4], in[5], in[6], in[7]);
-  or out1(y, in[2], in[3], in[6], in[7]);
-  or out0(z, in[1], in[3], in[5], in[7]);
+  or out2(x, d[4], d[5], d[6], d[7]);
+  or out1(y, d[2], d[3], d[6], d[7]);
+  or out0(z, d[1], d[3], d[5], d[7]);
   
-  and s2(o[2], x, en);
-  and s1(o[1], y, en);
-  and s0(o[0], z, en);
+  and s2(q[2], x, en);
+  and s1(q[1], y, en);
+  and s0(q[0], z, en);
 endmodule
